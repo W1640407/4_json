6d4d8eb0fbc570fd8fc816d75595847403577a89
Denis Voloshin <d.voloshin@infosec.ru>
2017-10-12 12:06:29 +0300

    initial commit

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,18 @@
+import codecs
 import json
+
+import sys
 
 
 def load_data(filepath):
-    pass
+    with codecs.open(filepath, 'r', encoding='utf-8', errors='ignore') as opened_file:
+        return opened_file.read()
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(opened_file):
+    print(json.dumps(json.loads(opened_file), sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    path_to_file = sys.argv[1]
+    pretty_print_json(load_data(path_to_file))
